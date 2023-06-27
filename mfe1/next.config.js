6186986d5e5c6c57af1f35fc76c72b2a9d4376fd
Ron Williams <nameof@amazon.com>
2023-06-27 15:21:45 -0400

    almost working... client side dynamic import working, SSR busted :\

--- mfe1/next.config.js
+++ mfe1/next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+const pac = require('./package.json');
+
+module.exports = {
+    webpack(config, options) {
+        Object.assign(config.experiments, { topLevelAwait: true });
+        // if (!options.isServer) {
+            config.plugins.push(
+            new NextFederationPlugin({
+                name: 'mfe1',
+                filename: 'static/chunks/mfe1.js',
+                // shared: Object.keys(pac.dependencies), // TODO - validate dependency sharing between MFEs and app
+                exposes: {
+                    "./thingy": "./src/app/thingy"
+                },
+                extraOptions: {
+                    verbose: true,
+                    automaticAsyncBoundary: true,
+                }
+            })
+            );
+        // }
+    
+        return config;
+    }
+};
---
+++ mfe1/src/app/thingy.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <div style={{border: '1px solid green'}}>here is the MFE aint it?</div>
+      </div>
+    </main>
+  )
+}
