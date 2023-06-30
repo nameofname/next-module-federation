3d759b7b3c7bb15f8adf35f0d298d6d5aee6db67
nameofname <nameofemail@gmail.com>
2023-06-30 14:01:49 -0400

    things are working now

--- mfe1/src/app/Mfe1.tsx
+++ mfe1/src/app/Mfe1.tsx
@@ -1,9 +1,7 @@
 export default function Mfe1() {
   return (
     <main>
-      <div>
-        <div style={{border: '1px solid green'}}>here is the MFE aint it?</div>
-      </div>
+      <div style={{border: '1px solid green'}}>here is the MFE aint it?</div>
     </main>
   )
 }
