80958efec2ad62d44a636a3d08828be2f739169c
Ron Williams <nameof@amazon.com>
2023-06-30 00:32:06 -0400

    i believe this is working now!!!!!!!!!!!!!

--- mfe1/src/app/page.tsx
+++ mfe1/src/app/page.tsx
@@ -1,10 +1,12 @@
 import styles from './page.module.css'
+import Mfe1 from './Mfe1';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div>
-        <div style={{border: '1px solid green'}}>here is the MFEEEEEE?</div>
+        <h1>Sandbox for MFE</h1>
+        <Mfe1 />
       </div>
     </main>
   )
