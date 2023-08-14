8adb65e6f7768f62c8b7d41d860e32d60ace371b
Ron Williams <nameof@amazon.com>
2023-08-14 11:49:42 -0400

    unifying formatting

--- mfe2/src/components/Mfe2Container.tsx
+++ mfe2/src/components/Mfe2Container.tsx
@@ -1,13 +1,30 @@
-import ClientComponent from './ClientComponent';
+import { useState, useEffect } from 'react';
 
 
 export default function Mfe2Container({ serverSideData }) {
+    const [ data, setData ] = useState(null);
+    useEffect(() => {
+        if (data === null) {
+            // artificial throttle : 
+            setTimeout(() => {
+                fetch("https://jsonplaceholder.typicode.com/todos/3")
+                .then(data => data.json())
+                .then(setData);
+            }, 1000);
+        }
+    });
+
     return (
         <main style={{border: '1px solid purple'}}>
             <h2>This is MFE 2</h2>
             <i>Here is server fetched data : </i>
             <code>{JSON.stringify(serverSideData)}</code>
-            <ClientComponent />
+            <div style={{border: '1px solid red'}}>
+                <i>Here is client fetched data : </i>
+                {
+                    !data ? (<p>loading...</p>) : (<code>{JSON.stringify(data, null, 4)}</code>)
+                }
+            </div>
         </main>
     )
 }
