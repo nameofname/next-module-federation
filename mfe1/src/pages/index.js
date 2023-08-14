4a4648a1639e5b3582a39a689415757b08eef2b2
Ron Williams <nameof@amazon.com>
2023-08-14 14:36:30 -0400

    reformatted to use different (worse) pattern, but no bugs

--- mfe1/src/pages/index.js
+++ mfe1/src/pages/index.js
@@ -1,13 +1,9 @@
 import MfeContainer from '../components/MfeContainer';
 
-/**
- * This is the entry point for the MFE, it's coded like a React Server Component
- * but we use a workaround to make it act like an RSC, since app router is 
- * not yet supported by nextjs-mf 
- * @returns a react component
- */
-export default async function() {
+export async function getServerSideProps() {
     let data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
     console.log('Mfe1 : fetching');
-    return (<MfeContainer data={await data.json()} />);
+    return await data.json();
 }
+
+export const Component = MfeContainer;
--- mfe2/src/pages/index.js
+++ mfe2/src/pages/index.js
@@ -1,8 +1,9 @@
 import Mfe2Container from '../components/Mfe2Container';
 
-export default async function Home() {
+export async function getServerSideProps() {
     let serverSideData = await fetch('https://jsonplaceholder.typicode.com/todos/2');
-    serverSideData = await serverSideData.json();
     console.log('Mfe2 : fetching');
-    return (<Mfe2Container serverSideData={serverSideData} />);
+    return await serverSideData.json();
 };
+
+export const Component = Mfe2Container;
