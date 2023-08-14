6589dd851379535bf852fd1f68568a76d9b4d7cc
Ron Williams <nameof@amazon.com>
2023-08-14 00:39:03 -0400

    cleaning up

--- mfe1/src/components/Mfe1.tsx
+++ mfe1/src/components/Mfe1.tsx
@@ -1,21 +1,28 @@
 import { useState } from 'react';
 import { Tooltip } from 'react-tooltip'
-// import ClientComponent from './ClientComponent';
+
+
+type Mfe1Props = {
+    data : any;
+}
 
 /**
  * Top level MFE component. 
  * This is a server component, so no interactivity can be programmed at this level
  * @returns 
  */
-export default function Mfe1() {
+export default function Mfe1({ data }: Mfe1Props) {
     const [ count, updateCount ] = useState(0);
 
     return (
-        <div>
+        <main style={{border: '1px solid green'}}>
+            <h2>This is MFE 1</h2>
+            <h2>Here is some fetched data : </h2>
+            <code>{JSON.stringify(data)}</code>
             <i data-tooltip-id="my-tooltip" data-tooltip-content="Tooltipz in ur eyez!">client component (tips have been toolified)</i>
             <Tooltip id="my-tooltip" />
             <p>current count: {count}</p>
             <button onClick={() => updateCount(count + 1)}>juice it</button>
-        </div>
+        </main>
     );
 }
