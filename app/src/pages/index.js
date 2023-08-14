d3c653cc2306ac4ab9180ce63321d7082167cb7e
nameofname <nameofemail@gmail.com>
2023-08-14 14:36:30 -0400

    reformatted to use different (worse) pattern, but no bugs

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,8 +1,7 @@
-import loadMfe from '../utils/loadMfe';
-const Mfe1 = loadMfe(import('mfe1/mfe'));
-const Mfe2 = loadMfe(import("mfe2/mfe"));
+const { getServerSideProps: fetchMfe1, Component: Mfe1 } = await import('mfe1/mfe');
+const { getServerSideProps: fetchMfe2, Component: Mfe2 } = await import('mfe1/mfe');
 
-const Home = () => {
+const Home = ({ mfe1Props, mfe2Props }) => {
     return (
         <div>
             <h1>This is the sample application</h1>
@@ -11,16 +10,16 @@ const Home = () => {
             <hr />
             <h2>below are the MFEs loaded via module federation : </h2>
             <hr />
-            <Mfe1 />
-            <Mfe2 />
+            <Mfe1 data={mfe1Props} />
+            <Mfe2 data={mfe2Props} />
         </div>
     );
 };
 
 export async function getServerSideProps(ctx) {
-    await Mfe1.refresh();
-    await Mfe1.refresh();
-    return { props: {}};
+    const mfe1Props = await fetchMfe1();
+    const mfe2Props = await fetchMfe2();
+    return { props: { mfe1Props, mfe2Props }};
 };
 
 export default Home;
